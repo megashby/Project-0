4cecbe9fe5196f28423a79ad487b19f3e3dcb11f
MariaKrychniak <32000559+MariaKrychniak@users.noreply.github.com>
2017-09-19 20:12:21 -0500

    Add files via upload

--- createaccount.php
+++ createaccount.php
@@ -2,34 +2,33 @@
 print<<<CREATE
 <html>
 <head>
+<link rel="stylesheet" href="./createaccount.css">
 <title> CREATE AN ACCOUNT </title>
+</head>
 <body>
-<p> This is the page to create an account </p>
+<p> Welcome to the CREATE AN ACCOUNT page </p>
 <table width = "75%">
 <form action = "./createaccount.php" method = "post">
 <tr>
-<td> Enter Email </td>
-<td> <input type = "text" name = "email" size = "30"/></td>
+<td> Enter Email </td> <td> <input type = "text" name = "email" size = "40"/></td>
 </tr>
 <tr>
-<td> Enter Password </td> 
-<td> <input type = "text" name = "password" size = "30"/></td>
+<td> Enter Password </td> <td> <input type = "text" name = "password" size = "40"/></td>
 </tr>
 <tr>
-<td> <input type = "submit" value = "Submit"/></td>
+<td><input class = "submit" type = "submit" value = "Submit"/></td>
 </tr>
 </form>
 </table>
+</body> 
 </html> 
 CREATE;
-
 extract ($_POST);
 $email = $_POST["email"];
 $pass = $_POST["password"];
 // echo $email;
 // echo $pass;
 // echo 'BLAAAHHH';
-
 $matches = Array();
 $emailfound = false;
 $fh = fopen("./passwd.txt", "r");
@@ -41,13 +40,11 @@ while (!feof($fh))
 	// echo ($line[0]);
 	// echo ($line[1]);
 	$matches[$line[0]] = $line[1];
-
 	// echo($line[0]);
 	// echo ";";
 	// echo ($line[1]);
 }
 fclose($fh);
-
 foreach ($matches as $key => $value)
 {
 	if ($email == $key)
@@ -56,12 +53,10 @@ foreach ($matches as $key => $value)
 	}
 }
 // echo $emailfound;
-
 if ($emailfound == true && $matches[$email]!= $pass && isset($_POST['email']) && isset($_POST["password"]))
 {
 	echo 'email already claimed';
 }
-
 if($emailfound == false && $_POST['email'] != "" && $_POST['password'] != '')
 {
 	$fh = fopen("./passwd.txt", 'a');
@@ -70,5 +65,4 @@ if($emailfound == false && $_POST['email'] != "" && $_POST['password'] != '')
 	header("Location: ./project.html");
 	exit();
 }
-
 ?>
--- login.php
+++ login.php
@@ -1,40 +1,34 @@
 <?php
-
 print <<<LOGIN2
 <html>
 <head> 
+<link rel="stylesheet" href="./login.css">
 <title> LOGIN </title>
 </head>
 <body>
-
-<p> This is the login page </p>
+<p> Welcome to the LOGIN Page </p>
 <table width = "75%"">
 <form action = "./login.php" method ="post">
 <tr>
-<td> Enter Email </td> <td> <input type = "text" name = "email" size = "30"/></td>
+<td> Enter Email </td> <td> <input type = "text" name = "email" size = "40"/></td>
 </tr>
 <tr>
-<td> Enter Password </td> <td> <input type = "text" name = "password" size = "30"/></td>
+<td> Enter Password </td> <td> <input type = "text" name = "password" size = "40"/></td>
 </tr>
 <tr>
-<td><input type = "submit" value = "Submit"/></td>
+<td><input class = "submit" type = "submit" value = "Submit"/></td>
 </tr>
-
 </form>
 </table>
 </body> 
-<html>
+</html>
 LOGIN2;
-
 extract ($_POST);
 $email = $_POST["email"];
 $pass = $_POST['password'];
-
 // echo $email;
 // echo $pass;
 // echo "\n";
-
-
 $matches = Array();
 $emailfound = false;
 $fh = fopen("./passwd.txt", 'r');
@@ -51,7 +45,6 @@ while (!feof($fh))
 	$matches[$line[0]] = $line[1];
 }
 fclose($fh);
-
 foreach ($matches as $key => $value)
 {
 	if ($email == $key)
@@ -69,11 +62,8 @@ if ($emailfound == true && $matches[$email]!= $pass && isset($_POST['email']) &&
 {
 	echo 'wrong password';
 }
-
 if ($emailfound == false && $email != "" && $pass != "")
 {
 	echo 'there is no account with that username';
 }
-
-
 ?>
