614fc1429be84a9c66dc030e3921e76502dbe4b7
Meg Ashby <megashby@utexas.edu>
2017-09-27 15:08:31 -0500

    Add files via upload
    These are two php files for storing images in the database and pulling images to display to the screen. I also have another php file to actually pull the images from the database. Once I have the information to connect to the MySQL server I will integrate these with the upload button on the html page

---
+++ store_image.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "cs.utexas.edu";
+$user = 'megashby';
+$pass = '';
+
+mysql_connect($host, $user, $pass);
+
+mysql_select_db('demo');
+
+$upload_image = $_FILES[' myimage'][ "name "];
+$folder = '/xampp/htdocs/images/';
+
+move_uploaded_file($_FILES[" myimage "][" tmp_name "], "$folder".$_FILES{" myimage "][" name "]);
+
+$insert_path = "INSERT INTO image_table VALUES('$folder', '$upload_image')";
+
+$var = mysql_query($insert_path);
+
+?>
