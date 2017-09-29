b36631151be22562c9ba1335a39dd01d88a2511a
Meg Ashby <megashby@utexas.edu>
2017-09-29 16:13:23 -0500

    Add files via upload

--- fetch_image.php
+++ fetch_image.php
@@ -1,27 +1,44 @@
 <?php
 
+$db_host = 'megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com:3306';
+
+$dsn = "mysql:host=megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com;post=3306;dbname=demo";
+$username= 'megashby';
+$password = 'mypassword';
+$name = 'demo';
+
+// mysql_connect("$db_host", "$username", "$password", TRUE) or die(mysql_error());
+$link = mysqli_connect('megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com', 'megashby', 'mypassword', 'demo', 3306);
+if (empty($link)){
+	die("mysqli connect failed".mysqli_connect_error());
+}
+else{
+	echo "JELLO";
+}
+
 #header('content-type:image/jpeg');
-$host = "localhost";
-$user = "root";
-$pass = " ";
-#$port = "3306";
-$dbs = 'megashby1';
+// $host = "localhost";
+// $user = "root";
+// $pass = " ";
+// #$port = "3306";
+// $dbs = 'megashby1';
 
-mysql_connect($host, $user, $pass);
+// mysql_connect($host, $user, $pass);
 
-mysql_select_db("megashby1");
+// mysql_select_db("demo");
 
 $name = $_GET['name'];
 
 $select_image = "select * from image_table";
 
-$var = mysql_query($select_image);
+$var = mysqli_query($link, $select_image);
+// print "<table border = '1'width = '75%'><tr><th> pic </th><th> name </th></tr>";
 
-while ($row = mysql_fetch_array($var))
+while ($row = $var ->fetch_row())
 {
 	$image_name = $row["imagename"];
 	$image_path = $row["imagepath"];
-	echo "img src=".$image_path"/"$image_name." width = 100 height = 100";
+	print "img src=".$image_path"/"$image_name." width = 100 height = 100";
 }
 
 ?>
--- getdata.php
+++ getdata.php
@@ -1,21 +1,46 @@
 <?php
-echo "fkjdskjlfsd";
-$host = 'localhost';
-$user = 'root';
-$pass = '';
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// // echo "jfsdjfjlkds";
 
 // mysql_connect($host, $user, $pass);
 
 // mysql_select_db('demo');
 
-// $imagename = $_FILES["myimage"]["name"];
+$db_host = 'megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com:3306';
+
+$dsn = "mysql:host=megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com;post=3306;dbname=demo";
+$username= 'megashby';
+$password = 'mypassword';
+$name = 'demo';
+
+// mysql_connect("$db_host", "$username", "$password", TRUE) or die(mysql_error());
+$link = mysqli_connect('megashby-instance1.cssjukptt0pw.us-east-2.rds.amazonaws.com', 'megashby', 'mypassword', 'demo', 3306);
+if (empty($link)){
+	die("mysqli connect failed".mysqli_connect_error());
+}
+else{
+	echo "JELLO";
+}
+
+$imagename = $_FILES["myimage"]["name"];
 
 // #Get the content of the image and then add slashes to it
-// $imagetmp = addslashes(file_get_contents($_FILES['myimage']['tmp_name']));
+$imagetmp = addslashes(file_get_contents($_FILES['myimage']['tmp_name']));
 
 // #insert the image name and image content in image_table
-// $insert_image = "INSERT INTO image_table VALUES('$imagetmp', '$imagename')";
+$insert_image = "INSERT INTO image_table VALUES('$imagetmp', '$imagename');";
+// echo $imagename;
+// echo $imagetmp;
+// 
+mysqli_query($link, $insert_image);
+mysqli_query($link, "COMMIT");
 
-// mysql_query($insert_image);
+// echo "HELLOOOO!!!!!!!";
+
+header("Location: logoutpage.php");
+
+
 
 ?>
